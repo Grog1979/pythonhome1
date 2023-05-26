cb29922e96adf7ac26c2d277bb964e49b829e2e6
Kostya <kosg@mail.ru>
2023-05-26 23:31:32 +0300

    home task

---
+++ 1.py
@@ -0,0 +1,13 @@
+#Найдите сумму цифр трехзначного числа.
+# n = int(input('enter number: '))
+# sum = 0
+# while n > 0:
+#   sum += n % 10
+#   n //= 10
+# print(sum)
+
+n = input('enter number: ')
+a = int(n[0])
+b = int(n[1])
+c = int(n[2])
+print(a+b+c)
